7e6686593acb5b374cb4425191eed0f891cc39c0
Yu Jin <yujin.yujin@gmail.com>
2025-04-17 20:55:37 +0800

    Add tici.proto and update proto.lock files (#362)
    * Add tici.proto and update proto.lock files
    
    * Update proto.lock
    
    * Fix tici proto build process with directory structure and compatibility fixes
    
    * Fix relative import in tici.pb.go
    
    * Update go.mod and go.sum for dependencies
    
    * fix the go complie

--- generate-go.sh
+++ generate-go.sh
@@ -8,8 +8,15 @@ go install github.com/gogo/protobuf/protoc-gen-gofast
 protoc -I.:${GOGO_PROTOBUF} \
     --gofast_out=plugins=grpc:../go-tipb *.proto
 
+# Generate TiCI proto with the main tipb Go package.
+protoc -I.:${GOGO_PROTOBUF} \
+    --gofast_out=plugins=grpc,Mtici/indexer.proto=github.com/pingcap/tipb/go-tipb,import_path=tipb:../go-tipb tici/*.proto
+
 cd ../go-tipb
+mv tici/indexer.pb.go tici.pb.go
 sed -i.bak -E 's/import _ \"gogoproto\"//g' *.pb.go
 sed -i.bak -E 's/import fmt \"fmt\"//g' *.pb.go
+sed -i.bak -E 's/import _ \"\.\"//g' *.pb.go
+sed -i.bak -E 's/package tipb_tici/package tipb/' *.pb.go
 rm -f *.bak
 go run golang.org/x/tools/cmd/goimports -w *.pb.go
